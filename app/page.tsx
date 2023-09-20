5a753e0511e3f95c05041a6c0d91761e4b509463
ManikantaPotnuru9176 <manikantapotnuru9176@gmail.com>
2023-09-20 15:40:53 +0530

    chore: added the preview and next buttons

---
+++ app/page.tsx
@@ -0,0 +1,11 @@
+"use client";
+import Quiz from "@/components/Quiz";
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div>
+      <Quiz />
+    </div>
+  );
+}
---
+++ components/Quiz.tsx
@@ -0,0 +1,64 @@
+import { useState } from "react";
+import { CssBaseline, Box, Container } from "@mui/material";
+import { lightBlue } from "@mui/material/colors";
+
+import QuestionCard from "./QuestionCard";
+import Result from "./Result";
+import questions from "../public/data/list";
+
+function Quiz() {
+  const [currentQuestionIndex, setCurrentQuestionIndex] = useState(0);
+  const [answers, setAnswers] = useState([]);
+  const finishedQuiz = currentQuestionIndex === questions.length;
+  const currentQuestion = questions[currentQuestionIndex];
+
+  const goToNext = () => {
+    setCurrentQuestionIndex((prevState) => prevState + 1);
+  };
+
+  const goToPrevious = () => {
+    setCurrentQuestionIndex((prevState) => prevState - 1);
+  };
+
+  const submitAnswer = (value) => {
+    if (!answers[currentQuestionIndex])
+      setAnswers((prevState) => [...prevState, value]);
+    goToNext();
+  };
+
+  const restartQuiz = () => {
+    setCurrentQuestionIndex(0);
+    setAnswers([]);
+  };
+
+  return (
+    <div>
+      <CssBaseline />
+      <Box
+        sx={{
+          backgroundColor: lightBlue[500],
+          height: "100vh",
+          display: "flex",
+          alignItems: "center",
+        }}
+      >
+        <Container maxWidth="sm">
+          {finishedQuiz ? (
+            <Result restartQuiz={restartQuiz} answers={answers} />
+          ) : (
+            <QuestionCard
+              question={currentQuestion}
+              questionNumber={currentQuestionIndex + 1}
+              submitAnswer={submitAnswer}
+              islastQuestion={currentQuestionIndex + 1 === questions.length}
+              goToPrevious={goToPrevious}
+              selectedValue={answers[currentQuestionIndex] || null}
+            />
+          )}
+        </Container>
+      </Box>
+    </div>
+  );
+}
+
+export default Quiz;
